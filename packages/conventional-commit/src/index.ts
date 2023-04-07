b26cfabd584821bcfa03a318803faa352f4a1053
kevits <4051421+kevits@users.noreply.github.com>
2023-04-07 23:01:03 +0200

    chore(conventional-commit): setup for publish (#2)

--- packages/conventional-commit/src/index.ts
+++ packages/conventional-commit/src/index.ts
@@ -20,6 +20,12 @@ const fullCommitExp: RegExp = /(?<header>.*)(?:\n{1,2})(?<body>(?<=\n{2})(?:[\w\
 const headerExp: RegExp = /(?<type>[\w-]+)(?:\((?<scope>[\w-]+)\))?(?<breaking>!)?: (?<description>(?!\s).+)$/
 const footerExp: RegExp = /^(?<token>BREAKING CHANGE|[a-zA-Z][a-zA-Z-]+)(?:: | (?=#))(?<message>[\w\W]+?(?=^(?:BREAKING CHANGE|[a-zA-Z][a-zA-Z-]+)(?:: | (?=#)))|[\S][\w\W]*)/gm // prettier-ignore
 
+/**
+ * Parse a full commit with header, body and footer and
+ * save those strings into a Commit type.
+ * @param commit the whole commit
+ * @returns a Commit type object
+ */
 export function parseFullCommit(commit: string): Commit | null {
     const match: RegExpMatchArray | null = commit.trim().match(fullCommitExp)
     if (match == null || match.groups == undefined) {
@@ -33,6 +39,11 @@ export function parseFullCommit(commit: string): Commit | null {
     }
 }
 
+/**
+ * Parse the header of a commit.
+ * @param header commit header
+ * @returns a CommitHeader type object
+ */
 export function parseHeader(header: string): CommitHeader | null {
     const match: RegExpMatchArray | null = header.trim().match(headerExp)
     if (match == null || match.groups == undefined) {
@@ -47,6 +58,11 @@ export function parseHeader(header: string): CommitHeader | null {
     }
 }
 
+/**
+ * Parse the footer of a commit.
+ * @param footer the footer of a commit
+ * @returns an array of CommitFooter type objects
+ */
 export function parseFooter(footer: string): Array<CommitFooter> {
     const matchIter: IterableIterator<RegExpMatchArray> = footer.trim().matchAll(footerExp)
     const footers: Array<CommitFooter> = []
@@ -64,6 +80,11 @@ export function parseFooter(footer: string): Array<CommitFooter> {
     return footers
 }
 
+/**
+ * Extracts the body of an full commit.
+ * @param fullCommit the whole commit string
+ * @returns the extracted body as string
+ */
 export function getBodyFromCommit(fullCommit: string): string {
     const commit: Commit | null = parseFullCommit(fullCommit)
     if (commit == null || commit.body == undefined) {
@@ -72,16 +93,33 @@ export function getBodyFromCommit(fullCommit: string): string {
     return commit.body.trim()
 }
 
+/**
+ * Converts a header string to a JSON string
+ * that follow the schema of the CommitHeader type.
+ * @param header commit header
+ * @returns JSON as string
+ */
 export function parseHeaderToJson(header: string): string {
     const parsedHeader: CommitHeader | null = parseHeader(header)
     return JSON.stringify(parsedHeader)
 }
 
+/**
+ * Converts a footer string to a JSON string
+ * that follows the schema of the CommitFooter type.
+ * @param footer commit footers
+ * @returns JSON array as string
+ */
 export function parseFooterToJson(footer: string): string {
     const parsedFooter: Array<CommitFooter> = parseFooter(footer)
     return JSON.stringify(parsedFooter)
 }
 
+/**
+ * Validates if the header follows the conventional commit specifications.
+ * @param headerToValidate commit header
+ * @returns wether it is a valid header or not
+ */
 export function validateHeader(headerToValidate: string): boolean {
     return headerExp.test(headerToValidate)
 }
